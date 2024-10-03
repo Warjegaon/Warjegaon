4fff5f605a8ce64316cb28fa9e83d759487ed4aa
WarjeGaon Pune <warjegaon@gmail.com>
2024-10-03 17:38:33 +0530

    Update 20241002-bibtya.js

--- Events/20241002-bibtya.js
+++ Events/20241002-bibtya.js
@@ -16,7 +16,7 @@ data.article=`
 
 मिरवणूकीचे प्रमुख आकर्षण : MK Lights Pune मार्फत लेझर शो आणि संगीत मैफल (डीजे) आयोजित केलेला आहे.
 
-<iframe width="800" height="600"
+<iframe 
 src="https://www.youtube.com/embed/wszHNgiicIs">
 </iframe>
 
